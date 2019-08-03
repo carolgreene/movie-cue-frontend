0910a1d69c9f3fdd469520e422f2656a465e602d
carolgreene <carolgreene3@gmail.com>
2019-08-03 06:38:35 -0700

    made basic form

--- src/components/movies/MovieForm.js
+++ src/components/movies/MovieForm.js
@@ -6,7 +6,31 @@ class MovieForm extends Component {
   render() {
     return (
       <div>
-        This is Movie Form!
+        <h3>Enter Movie</h3>
+        <form >
+          <input 
+            type="text"
+            name="title"
+            placeholder="title"
+          /><br/>
+          <input
+            type="text"
+            name="genre"
+            placeholder="genre"
+          /><br/>
+          <input 
+            type="text"
+            name="rating"
+            placeholder="rating"
+          /><br/>
+          <input
+            type="textarea"
+            name="description"
+            placeholder="description"
+          /><br/>
+
+          <input type="submit" />
+        </form>
       </div>
     )
   }
