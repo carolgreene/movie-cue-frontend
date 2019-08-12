2422283af49b9d4473f5eab862c4a1186b8d1808
carolgreene <carolgreene3@gmail.com>
2019-08-11 18:04:54 -0700

    made a controlled form & imported connect

--- src/components/movies/MovieForm.js
+++ src/components/movies/MovieForm.js
@@ -1,32 +1,61 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+//import {addMovie} from '.../actions/addMovie'
+
 
 class MovieForm extends Component {
   
+  state = {
+    title: '', 
+    genre: '', 
+    rating: '', 
+    description: ''
+  }
+
+  
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value    //need brackets bc it will read the key forthe object 
+    })                                           //& set the value for it. w/o brackets it assigns the
+  }                                             //key a name of event.target.name 
+  
+  handleSubmit = (event) => {
+    debugger;
+  }
 
   render() {
     return (
       <div>
         <h3>Enter Movie</h3>
-        <form >
+        <form onSubmit={this.handleSubmit}>
           <input 
             type="text"
             name="title"
             placeholder="title"
+            value={this.state.title}
+            onChange={this.handleChange}
           /><br/>
           <input
             type="text"
             name="genre"
             placeholder="genre"
+            value={this.state.genre}
+            onChange={this.handleChange}
           /><br/>
           <input 
             type="text"
             name="rating"
             placeholder="rating"
+            value={this.state.rating}
+            onChange={this.handleChange}
           /><br/>
           <input
             type="textarea"
             name="description"
             placeholder="description"
+            value={this.state.description}
+            onChange={this.handleChange}
           /><br/>
 
           <input type="submit" />
@@ -36,4 +65,4 @@ class MovieForm extends Component {
   }
 }
 
-export default MovieForm
+export default connect(null)(MovieForm)
