a5e968e9599c34c680e48ee17050d903414461f0
carolgreene <carolgreene3@gmail.com>
2019-08-15 18:57:14 -0700

    completed addMovie to fetch POST properly

--- src/actions/addMovie.js
+++ src/actions/addMovie.js
@@ -1,16 +1,27 @@
 export const addMovie = (data) => {
-    //debugger;
+    //console.log(data)
   return (dispatch) => {
-    fetch('http://10.0.0.99:3001/api/v1/movies', {
+    //const sendableMovieData = {      Don't need to use this here bc attributes are only 1 word
+      //title: data.title,             Will need to use this in the review action creator
+     // genre: data.genre,
+      //rating: data.rating,
+      //description: data.description
+    //}
+    //console.log(sendableMovieData)
+    return fetch('http://10.0.0.99:3001/api/v1/movies', {
+      credentials: "include",
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       }, 
-      method: 'POST',
+      
       body: JSON.stringify(data)
     })
     .then(response => response.json())
-    .then(movie => dispatch({type: 'ADD_MOVIE', payload: movie}))
+    //.then(console.log)
+    .then(movie => dispatch({type: 'ADD_MOVIE', payload: movie.data}))
+    
   }
 
   
