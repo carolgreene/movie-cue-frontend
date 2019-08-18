ca1e659af5da285557d4689b22dac3b20be2f1cf
carolgreene <carolgreene3@gmail.com>
2019-08-18 16:13:47 -0700

    imported MovieForm & rendered it

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import ReviewForm from '../components/reviews/ReviewForm'
 
 class ReviewsContainer extends Component {
   
   render() {
     return (
       <div>
-        This is the Reviews Container!
+        <ReviewForm />
       </div>
     )
   }
