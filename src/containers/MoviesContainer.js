1c0b3e444f2b36d01c6f11f6f5158fc453a49a10
carolgreene <carolgreene3@gmail.com>
2019-08-18 14:00:30 -0700

    imported Route & Switch & wrapped routes w/Switch

--- src/containers/MoviesContainer.js
+++ src/containers/MoviesContainer.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import MovieForm from '../components/movies/MovieForm';
 import Movies from '../components/movies/Movies';
 import { fetchMovies } from '../actions/fetchMovies'
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import MovieCard from '../components/movies/MovieCard'
 
 class MoviesContainer extends Component {
@@ -15,11 +15,12 @@ class MoviesContainer extends Component {
   
   render() {
     return (
-      <div>        
-        <Route path='/movies/new' component={MovieForm}/>  
-        <Route path='/movies/:id' render={(routerProps) => <MovieCard {...routerProps} movies={this.props.movies}/>}/>      
-        <Route exact path='/movies' render={(routerProps) => <Movies {...routerProps} movies={this.props.movies}/>}/>
-        
+      <div>     
+        <Switch>   {/* will match only the 1st matching route, not multiple routes. :id must be below new*/}
+          <Route path='/movies/new' component={MovieForm}/>  
+          <Route path='/movies/:id' render={(routerProps) => <MovieCard {...routerProps} movies={this.props.movies}/>}/>      
+          <Route exact path='/movies' render={(routerProps) => <Movies {...routerProps} movies={this.props.movies}/>}/>
+        </Switch>
       </div>
     )
   }
