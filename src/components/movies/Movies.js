faf31f1208623e395efe2468d0086ebb68643cdb
carolgreene <carolgreene3@gmail.com>
2019-08-18 14:01:44 -0700

    made list of movies links & imported Link

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom'
 import MovieCard from './MovieCard';
 
 const Movies = (props) => {
@@ -7,7 +8,9 @@ const Movies = (props) => {
   return (
     <div>
       {props.movies.map(movie => 
-        <div key={movie.id}><MovieCard movie={movie}/></div>)}
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.attributes.title}</Link>
+        </li>)}
       
     </div>
   )  
