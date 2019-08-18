e71de21a0a4a448d03d260591724b84317664011
carolgreene <carolgreene3@gmail.com>
2019-08-18 14:07:25 -0700

    imported ReviewsContainer & return it on MovieCard

--- src/components/movies/MovieCard.js
+++ src/components/movies/MovieCard.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import ReviewsContainer from '../../containers/ReviewsContainer';
 
 const MovieCard = (props) => {
   console.log(props)
 
-  let movie = props.movies[props.match.params.id - 1]
-  console.log(movie)
+  let movie = props.movies[props.match.params.id - 1]  //this won't work if items have been deleted & index is
+  console.log(movie)                                   //not necessarily 1 less than the id number
 
   return (
-    
     <div>
+    <h2>
       {movie ? movie.attributes.title : null}
+    </h2>
+    <ReviewsContainer />
     </div>
   )
   
