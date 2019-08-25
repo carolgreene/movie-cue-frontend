3adf2ac6f932d82fb989ad496ffcce00e7506dab
carolgreene <carolgreene3@gmail.com>
2019-08-25 14:59:01 -0700

    added Reviews to div & passed props to Reviews & ReviewForm

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import ReviewForm from '../components/reviews/ReviewForm'
+import Reviews from '../components/reviews/Reviews'
 
 class ReviewsContainer extends Component {
   
   render() {
     return (
       <div>
-        <ReviewForm />
+        <Reviews reviews={this.props.movie && this.props.movie.attributes.reviews}/>
+        <ReviewForm movie={this.props.movie}/>
       </div>
     )
   }
