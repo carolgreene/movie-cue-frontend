6dcbfc05cecc9a5a58c898f457f7fb10d05602e5
carolgreene <carolgreene3@gmail.com>
2019-08-25 14:46:02 -0700

    added on_change & handle_change

--- src/components/reviews/ReviewForm.js
+++ src/components/reviews/ReviewForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-//import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+import {addReview} from '../../actions/addReview';
 
 class ReviewForm extends Component {
 
@@ -8,22 +9,40 @@ class ReviewForm extends Component {
     comments: ''
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault() 
+    this.props.addReview(this.state, this.props.movie.attributes.id)
+    this.setState({
+      rating: '',
+      comments: ''
+    })
+  
+  }
+
   render() {
     return (
       <div>
         <h4>Write Review</h4>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             type="text"
             name="rating"
             placeholder="rating"
             value={this.state.rating}
+            onChange={this.handleChange}
           /><br/>
           <input
             type="textarea"
             name="comments"
             placeholder="comments"
             value={this.state.comments}
+            onChange={this.handleChange}
           /><br/>
 
           <input type="submit" />
@@ -33,4 +52,4 @@ class ReviewForm extends Component {
   }
 }
 
-export default ReviewForm
+export default connect(null, {addReview})(ReviewForm)
