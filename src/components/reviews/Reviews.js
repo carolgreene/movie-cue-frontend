b95c0fb2430a258c2ac1a8f956d1d081ad1b4e6a
carolgreene <carolgreene3@gmail.com>
2019-08-28 18:53:26 -0700

    added delete button & handler. Rcvd movie props

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,20 +1,28 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {deleteReview} from '../../actions/deleteReview.js'
 
 const Reviews = (props) => {
-//console.log(props)
+console.log("props for Reviews:", props)
 
 const reviews = props.reviews
-console.log("reviews:",reviews)
+//console.log("reviews:",reviews)
+
+const movie = props.movie
+
+const handleDelete = (review, movie) => {  
+  props.deleteReview(review.id, movie.attributes.id)
+}
 
 
   return (
     <div>
       <h3>Reviews</h3>
       {reviews && reviews.map(review =>  //1st time it runs it's undefined
-         <li key={review.id}>{review.rating} Stars- {review.comments}</li>
+         <li key={review.id}>{review.rating} Stars- {review.comments} <button onClick={() => handleDelete(review, movie)}>Delete</button></li>
         )}
     </div>
   )
 }
 
-export default Reviews
+export default connect(null, {deleteReview})(Reviews)
