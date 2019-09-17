3e4f7f1eb6211d32595a78e243052d4101d8265b
carolgreene <carolgreene3@gmail.com>
2019-09-17 14:54:38 -0700

    rcv movie prop from route, make edit & review links, & trim conditional stmt

--- src/components/movies/MovieCard.js
+++ src/components/movies/MovieCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReviewsContainer from '../../containers/ReviewsContainer';
+import {Link} from 'react-router-dom'
 
 const MovieCard = (props) => {
   console.log("props:", props)
   console.log("match:", props.match.params.id[0])
 
-  let movie = props.movies.filter(movie => movie.id == props.match.params.id)[0]  //can use either this or below
+  //let movie = props.movies.filter(movie => movie.id == props.match.params.id)[0]  //can use either this or below
                                                                                             //this finds movie based on id
-                                                                                            //returns an array w/just 1 movie in it
+    let movie = props.movie                                                                                        //returns an array w/just 1 movie in it
                                                                                             //that's why you need [0] so it will only
                                                                                             //return the movie
 
@@ -15,13 +16,24 @@ const MovieCard = (props) => {
   //console.log(movie)                                   //not necessarily 1 less than the id number
                                                         //this finds movie based on index
   return (
+    movie ?    
     <div>
-    <h2>{movie ? movie.attributes.title : null}</h2>
-    <li>{movie ? movie.attributes.rating : null}</li>
-    <li>{movie ? movie.attributes.runtime  : null}</li>
-    <li>{movie ? movie.attributes.description : null}</li>
-    <ReviewsContainer movie={movie}/>   {/*give ReviewsContainer the movie prop. Will then be given to the ReviewForm*/}
-    </div>
+      <h2>{movie.attributes.title}</h2>
+      <li>{movie.attributes.rating}</li>
+      <li>{movie.attributes.runtime}</li>
+      <li>{movie.attributes.description}</li>    
+    
+    <Link to={`/movies/${movie.id}/edit`}>Edit this Movie</Link>
+    {movie.attributes.reviews.map(review =>
+    <li key={review.id}>
+    <Link to={`/movies/${movie.id}/reviews/${review.id}`}>{review.rating} Stars</Link>
+    </li>
+    )
+  }
+    
+    {/*<ReviewsContainer movie={movie}/>   give ReviewsContainer the movie prop. Will then be given to the ReviewForm*/}
+    </div> :
+    null
   )
   
 }
