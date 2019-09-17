bcb2faf9359a6cbef882fb98ee20d9e35abb3265
carolgreene <carolgreene3@gmail.com>
2019-09-17 15:10:30 -0700

    added link for review

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {deleteReview} from '../../actions/deleteReview.js'
+import {Link} from 'react-router-dom'
 
 const Reviews = (props) => {
 console.log("props for Reviews:", props)
@@ -19,7 +20,8 @@ const handleDelete = (review, movie) => {
     <div>
       <h3>Reviews</h3>
       {reviews && reviews.map(review =>  //1st time it runs it's undefined
-         <li key={review.id}>{review.rating} Stars- {review.comments} <button onClick={() => handleDelete(review, movie)}>Delete</button></li>
+         <li key={review.id}>
+         <Link to={`/movies/${movie.id}/reviews/${review.id}`}>{review.rating} Stars</Link>{/*- {review.comments} <button onClick={() => handleDelete(review, movie)}>Delete</button>*/}</li>
         )}
     </div>
   )
