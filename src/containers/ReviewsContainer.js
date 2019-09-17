180b9fdb9df211cc42507fe1e76ed5b913c0acf4
carolgreene <carolgreene3@gmail.com>
2019-09-17 15:28:10 -0700

    comments for ReviewCard

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import ReviewForm from '../components/reviews/ReviewForm'
 import Reviews from '../components/reviews/Reviews'
+import ReviewCard from '../components/reviews/ReviewCard'
+
 
 class ReviewsContainer extends Component {
   
   render() {
+
     return (
+     
       <div>
         <Reviews reviews={this.props.movie && this.props.movie.attributes.reviews} movie={this.props.movie}/>
         <ReviewForm movie={this.props.movie}/>
+        <ReviewCard /> {/* reviews={this.props.movie && this.props.movie.attributes.reviews} movie={this.props.movie}/>*/}
       </div>
     )
   }
