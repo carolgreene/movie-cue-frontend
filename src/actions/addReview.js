8db8d23054eb6b4adec8810a8afec488bde440ca
carolgreene <carolgreene3@gmail.com>
2019-09-19 16:43:55 -0700

    added sendable data to correct for camelCase vs snake_case

--- src/actions/addReview.js
+++ src/actions/addReview.js
@@ -1,12 +1,19 @@
 export const addReview = (review, movieId) => {
   
   return (dispatch) => {
+    const sendableReviewData = {              //had to add this bc I added fields w/2 word. React is camelCase, rails is snake_case
+      reviewer_name: review.reviewerName,
+      review_title: review.reviewTitle,
+      rating: review.rating,
+      comments: review.comments,
+      movie_id: review.movieId
+    }
     fetch(`http://10.0.0.99:3001/api/v1/movies/${movieId}/reviews`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(review)
+      body: JSON.stringify(sendableReviewData)
     })
     .then(r => r.json())
     .then(movie =>  {
