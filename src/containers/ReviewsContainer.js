efec98bdaaeed4eed9fafd63571a8de536497019
carolgreene <carolgreene3@gmail.com>
2019-09-19 16:52:24 -0700

    commented out ReviewCard. Will get from Reviews

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReviewForm from '../components/reviews/ReviewForm'
 import Reviews from '../components/reviews/Reviews'
-import ReviewCard from '../components/reviews/ReviewCard'
+//import ReviewCard from '../components/reviews/ReviewCard'
 
 
 class ReviewsContainer extends Component {
@@ -13,7 +13,7 @@ class ReviewsContainer extends Component {
       <div>
         <Reviews reviews={this.props.movie && this.props.movie.attributes.reviews} movie={this.props.movie}/>
         <ReviewForm movie={this.props.movie}/>
-        <ReviewCard /> {/* reviews={this.props.movie && this.props.movie.attributes.reviews} movie={this.props.movie}/>*/}
+        {/*<ReviewCard /> {/* reviews={this.props.movie && this.props.movie.attributes.reviews} movie={this.props.movie}/>*/}
       </div>
     )
   }
