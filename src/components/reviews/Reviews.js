5eadce5bd6f548cb5c781effe321acfdacf98ef3
carolgreene <carolgreene3@gmail.com>
2019-09-19 16:47:32 -0700

    added review title to link

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -21,7 +21,7 @@ const handleDelete = (review, movie) => {
       <h3>Reviews</h3>
       {reviews && reviews.map(review =>  //1st time it runs it's undefined
          <li key={review.id}>
-         <Link to={`/movies/${movie.id}/reviews/${review.id}`}>{review.rating} Stars</Link>{/*- {review.comments} <button onClick={() => handleDelete(review, movie)}>Delete</button>*/}</li>
+         <Link to={`/movies/${movie.id}/reviews/${review.id}`}>{review.review_title}  {review.rating} Stars</Link>{/*- {review.comments} <button onClick={() => handleDelete(review, movie)}>Delete</button>*/}</li>
         )}
     </div>
   )
