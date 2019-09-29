6e609073c5c9b332aa8802ed6135ffb71fc466a3
carolgreene <carolgreene3@gmail.com>
2019-09-29 09:19:56 -0700

    added className & a header

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -6,7 +6,8 @@ const Movies = (props) => {
   
   return (
     
-    <div>
+    <div className="movies">
+      <h2>Movies</h2>
       {props.movies.map(movie => 
         <li key={movie.id}>
           <Link to={`/movies/${movie.id}`}>{movie.attributes.title}</Link>
