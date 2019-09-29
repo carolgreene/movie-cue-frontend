0510be96c781895728b7674529e3d92781d3c3b4
carolgreene <carolgreene3@gmail.com>
2019-09-29 09:11:23 -0700

    playing w/adding & taking out NavigationBar & ReviewsContainer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import MoviesContainer from './containers/MoviesContainer';
 //import ReviewsContainer from './containers/ReviewsContainer';
 import { connect } from 'react-redux'  //will go wherever we need to have access to the store
+//import Home from './components/Home.js'
+//import NavigationBar from './components/NavigationBar.js'
 
 class App extends React.Component {
 
@@ -12,8 +14,9 @@ class App extends React.Component {
   render() {
   return (
     <div>
+      {/*<NavigationBar />*/}
       <MoviesContainer />
-      
+      {/*<ReviewsContainer />*/}
     </div>
   )
  }
