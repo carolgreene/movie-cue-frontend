cadec9c9539332831586aed5f9b0688358e55cae
carolgreene <carolgreene3@gmail.com>
2019-09-29 09:32:09 -0700

    added navigationBar & jumbotron & commented out renderMerged & propsRoute

--- src/containers/MoviesContainer.js
+++ src/containers/MoviesContainer.js
@@ -3,11 +3,14 @@ import { connect } from 'react-redux';
 import MovieForm from '../components/movies/MovieForm';
 import Movies from '../components/movies/Movies';
 import { fetchMovies } from '../actions/fetchMovies'
-import {Route, Switch, NavLink} from 'react-router-dom'
+import  {BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom'
 import MovieCard from '../components/movies/MovieCard'
 import Home from '../components/Home'
 import Reviews from '../components/reviews/Reviews'
 import ReviewCard from '../components/reviews/ReviewCard'
+import { NavigationBar } from '../components/NavigationBar'
+import { Layout } from '../components/Layout'
+import { Jumbotron } from '../components/Jumbotron'
 
 class MoviesContainer extends Component {
 
@@ -20,7 +23,7 @@ class MoviesContainer extends Component {
   
   render() {
 
-    const renderMergedProps = (component, ...rest) => {
+    {/*const renderMergedProps = (component, ...rest) => {
       const finalProps = Object.assign({}, ...rest);
       return (
         React.createElement(component, finalProps)
@@ -33,44 +36,47 @@ class MoviesContainer extends Component {
           return renderMergedProps(component, routeProps, rest);
         }}/>
       );
-    }
+    }*/}
 
     const { movies } = this.props  //**ck this later to see if I can get Howard's way to work instead of getting movie w/in MC component
-    return (
-      <div className="MoviesContainer">
-        <div>
-          <NavLink exact to='/movies'>Movies |</NavLink>
-          <NavLink exact to='/movies/new'>Add Movie |</NavLink>
-          <NavLink exact to='/'>Home </NavLink>
-        </div>
-           
-        <Switch>   {/* will match only the 1st matching route, not multiple routes. :id must be below new*/}
+    return (      
+    
+      <React.Fragment>
+        <Router>
+          <NavigationBar />
+          <Jumbotron />
+            {/*  <NavLink exact to='/movies'>Movies |</NavLink>
+            <NavLink exact to='/movies/new'>Add Movie |</NavLink>
+            <NavLink exact to='/'>Home </NavLink>*/}
+        {/*</div>*/}
+         <Layout>
+          <Switch>   {/* will match only the 1st matching route, not multiple routes. :id must be below new*/}
         
-        <Route exact path='/' component={Home}/>
-          <Route path='/movies/new' component={MovieForm}/>  {/*router props are automatically passed when you use component*/}
-          <Route exact path='/movies/:id/edit' render={props => {
-            const movie = movies.find(movie => movie.id === props.match.params.id)
-            console.log(movie)
-            return <MovieForm movie={movie} {...props}/>
-          }
-        }/>
+            <Route exact path='/' component={Home}/>
+            <Route path='/movies/new' component={MovieForm}/>  {/*router props are automatically passed when you use component*/}
+            <Route exact path='/movies/:id/edit' render={props => {
+              const movie = movies.find(movie => movie.id === props.match.params.id)
+              console.log(movie)
+              return <MovieForm movie={movie} {...props}/>
+            }
+          }/>
           {/*<Route exact path='/movies/:id' render={(routerProps) => <MovieCard {...routerProps} movies={this.props.movies}/>}/>*/}      
-          <Route exact path='/movies' render={(routerProps) => <Movies {...routerProps} movies={this.props.movies}/>}/>
-        {/*Howard did*/}
-           <Route exact path='/movies/:id' render={props => {
-             const movie = movies.find(movie => movie.id == props.match.params.id)
-             console.log("route:", movie)
-           return <MovieCard movie={movie} {...props} movies={this.props.movies}/>
-           }
+            <Route exact path='/movies' render={(routerProps) => <Movies {...routerProps} movies={this.props.movies}/>}/>
+          {/*Howard did*/}
+            <Route exact path='/movies/:id' render={props => {
+              const movie = movies.find(movie => movie.id == props.match.params.id)
+              console.log("route:", movie)
+              return <MovieCard movie={movie} {...props} movies={this.props.movies}/>
+            }
           }/>
           {/*<Route path='/movies/:id/reviews' render={(routerProps) => <MovieCard {...routerProps} movies={this.props.movies}/>}/>*/}
-          <Route exact path='/movies/:movie_id/reviews/:id' render={props => {
-            const matchingMovie = movies.find(movie => movie.id === props.match.params.movie_id)
+            <Route exact path='/movies/:movie_id/reviews/:id' render={props => {
+              const matchingMovie = movies.find(movie => movie.id === props.match.params.movie_id)
             {/*const matchingReview = matchingMovie.attributes.reviews.find(review => review.id === props.match.params.review.id)*/}
-            
-            console.log("matchingMovie", matchingMovie)             
-          return <ReviewCard movie={matchingMovie} {...props} movies={this.props.movies}/>
-          }
+              
+              console.log("matchingMovie", matchingMovie)             
+              return <ReviewCard movie={matchingMovie} {...props} movies={this.props.movies}/>
+            }
         }/>
            
             {/*return <ReviewCard movie={movie} {...props} reviews={this.props.movie && this.props.movie.attributes.reviews}/>
@@ -79,11 +85,15 @@ class MoviesContainer extends Component {
           {/*<PropsRoute path='/movies/:movie_id/reviews/:id' component={ReviewCard} movies={this.props.movies}/>*/}
         
          {/*} <Route path='/movies/:movie_id/reviews/:id' render={routeProps => <ReviewCard {...routeProps} movies={movies}/>} />*/}
-          <Route exact path='movies/:movie_id/reviews' component={Reviews}/>
+            <Route exact path='movies/:movie_id/reviews' component={Reviews}/>
         
 
-        </Switch>
-      </div>
+          </Switch>
+        </Layout>
+      </Router>
+    </React.Fragment>
+          
+      
     )
   }
 }
