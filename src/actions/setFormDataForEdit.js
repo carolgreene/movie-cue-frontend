5e3d3f90e9e18e0699b377a7f69f1d6c0f7cf666
carolgreene <carolgreene3@gmail.com>
2019-10-06 14:39:23 -0700

    added action to fill form for edit

---
+++ src/actions/setFormDataForEdit.js
@@ -0,0 +1,16 @@
+export const setFormDataForEdit = movie => {
+    console.log("edit action", movie)
+  const movieFormData = {
+    title: movie.attributes.title,
+    genre: movie.attributes.genre,
+    rating: movie.attributes.rating,
+    runtime: movie.attributes.runtime,
+    description: movie.attributes.description
+  }
+  console.log("in action", movieFormData)
+
+  return {
+    type: "SET_FORM_DATA_FOR_EDIT",
+    movieFormData
+  }
+}
