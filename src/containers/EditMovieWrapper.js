9bfe66e7b0469c8afc2325337dc0912b5e220d3b
carolgreene <carolgreene3@gmail.com>
2019-10-07 04:22:26 -0700

    adusted spacing & reducer. edit working now

--- src/containers/EditMovieWrapper.js
+++ src/containers/EditMovieWrapper.js
@@ -5,41 +5,38 @@ import { setFormDataForEdit } from '../actions/setFormDataForEdit'
 import { connect } from 'react-redux'
 import { resetMovieForm } from '../actions/resetMovieForm'
 
-class EditMovieWrapper extends React.Component {           
- 
+class EditMovieWrapper extends React.Component {      
   
     
-   componentDidMount(){
-     console.log("in edit wrapper: history", this.props.history)
-     this.props.movie && this.props.setFormDataForEdit(this.props.movie)
-   }
+  componentDidMount(){
+    console.log("in edit wrapper: history", this.props.history)
+    this.props.movie && this.props.setFormDataForEdit(this.props.movie)
+  }
 
-   componentDidUpdate(prevProps) {
-     this.props.movie && !prevProps.movie &&this.props.setFormDataForEdit(this.props.movie)
-   }
+  componentDidUpdate(prevProps) {
+    this.props.movie && !prevProps.movie &&this.props.setFormDataForEdit(this.props.movie)
+  }
 
-   componentWillUnmount() {
-     this.props.resetMovieForm()
-   }
+  componentWillUnmount() {
+    this.props.resetMovieForm()
+  }
 
+  handleSubmit = (formData) => {
+    //console.log("IN HANDLE SUBMIT", history)
+    const {updateMovie, movie, history} = this.props
+    console.log("IN HANDLE SUBMIT: HISTORY", history)
+    updateMovie({
+      ...formData,
+      movieId: movie.id}, 
+      history)                //can also resetMovieForm here instead of in addMovie action creator by
+                              //adding on .then(() => {
+                              //    resetMovieForm() after history) importing & exporting & deconstructing it
+  } 
 
-    handleSubmit = (formData) => {
-      //console.log("IN HANDLE SUBMIT", history)
-      const {updateMovie, movie, history} = this.props
-      console.log("IN HANDLE SUBMIT: HISTORY", history)
-      updateMovie({
-        ...formData,
-        movieId: movie.id}, 
-        history)                //can also resetMovieForm here instead of in addMovie action creator by
-                                //adding on .then(() => {
-                                //    resetMovieForm() after history) importing & exporting & deconstructing it
-                      
-    } 
-
-    render() {
-      const {history, movie} = this.props
-      return <MovieForm editMode handleSubmit={this.handleSubmit} /> 
-    } 
+  render() {
+    const {history, movie} = this.props
+    return <MovieForm editMode handleSubmit={this.handleSubmit} /> 
+  } 
         
 }  
 
