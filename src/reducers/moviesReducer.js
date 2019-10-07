f774939f238bbff484ed611dc6eb9db0d5f82574
carolgreene <carolgreene3@gmail.com>
2019-10-07 04:13:04 -0700

    changed updateMovie to if statement. working now

--- src/reducers/moviesReducer.js
+++ src/reducers/moviesReducer.js
@@ -14,7 +14,17 @@ export default function moviesReducer(state = initialState, action) {
 
     case 'UPDATE_MOVIE':
     console.log("reducer, update movie", action.payload)  
-      return state.movies.map(movie => movie.id === action.payload.movieId ? action.payload : movie)
+      //return state.movies.map(movie => movie.id === action.payload.id ? action.payload : movie)
+      let movies1 = state.movies.map(movie => {
+        if (movie.id === action.payload.id) {
+          return action.payload
+        } else {
+          return movie
+        }
+      })
+      return {...state, movies: movies1}
+
+
 
     case 'ADD_REVIEW':
     console.log("reducer-add review:", action.payload)
