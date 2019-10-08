b1bc52648b89efe12b229ad66ae83036722f81e8
carolgreene <carolgreene3@gmail.com>
2019-10-08 15:55:38 -0700

    cleaned up spacing

--- src/containers/NewMovieWrapper.js
+++ src/containers/NewMovieWrapper.js
@@ -5,23 +5,18 @@ import { connect } from 'react-redux'
 
 const NewMovieWrapper = ({ history, addMovie }) => {
 
+  const handleSubmit = (formData, history) => {
+    addMovie({
+      ...formData}, 
+      history)                //can also resetMovieForm here instead of in addMovie action creator by
+                              //adding on .then(() => {
+                              //    resetMovieForm() after history) importing & exporting & deconstructing it
+                    
+  } 
 
-    const handleSubmit = (formData, history) => {
-      //event.preventDefault()
-      addMovie({
-        ...formData}, 
-        history)                //can also resetMovieForm here instead of in addMovie action creator by
-                                //adding on .then(() => {
-                                //    resetMovieForm() after history) importing & exporting & deconstructing it
-                      
-    } 
-    return <MovieForm history={history} handleSubmit={handleSubmit} />  
+
+  return <MovieForm history={history} handleSubmit={handleSubmit} />  
         
 }
-
-  
-  
-
-
 
 export default connect(null, {addMovie})(NewMovieWrapper)
