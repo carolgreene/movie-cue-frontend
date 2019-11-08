963fbb798b6a269fbcc12b888922ec27204ebf7d
carolgreene <carolgreene3@gmail.com>
2019-11-08 15:15:40 -0800

    added return before fetch

--- src/actions/addReview.js
+++ src/actions/addReview.js
@@ -8,14 +8,14 @@ export const addReview = (review, movieId) => {
       comments: review.comments,
       movie_id: review.movieId
     }
-    fetch(`http://10.0.0.99:3001/api/v1/movies/${movieId}/reviews`, {
+    return fetch(`http://10.0.0.99:3001/api/v1/movies/${movieId}/reviews`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(sendableReviewData)
     })
-    .then(r => r.json())
+    .then(response => response.json())
     .then(movie =>  {
       if (movie.error) {
         alert(movie.error)
