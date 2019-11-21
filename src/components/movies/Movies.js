5e6cabdbfddacf97f00a924e7bb0628dd6ab5cac
carolgreene <carolgreene3@gmail.com>
2019-11-20 17:01:21 -0800

    spacing

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -5,8 +5,8 @@ import {Link} from 'react-router-dom'
 class Movies extends Component {  
 
   constructor(props) {
-  super(props)
-  this.state = { }
+    super(props)
+    this.state = { }
 
   }
   
