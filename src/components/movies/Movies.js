71388bc8c5fd7893a3b6e8943e16cd0b71f53bd4
carolgreene <carolgreene3@gmail.com>
2019-11-20 16:06:25 -0800

    like button working

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -1,11 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
 
-//need to figure out how to load the initial info into likes state for the movie_id & count of 0 for each movie.
-//Then update only the count of the id that has been clicked while keeping the rest the same.
-//possibly thinking of having a hash w/keys of id & count.
 
-//I think I might be on to something here but push is not working yet. 
 class Movies extends Component {  
 
   constructor(props) {
@@ -14,44 +10,28 @@ class Movies extends Component {
 
   }
   
-    
-    
-    
-  
 
-  
-//incrementing is working!! Just need to figure out how to show the likes on the screen correctly
   handleLikes = (e) => {
-    let newLikes
-    console.log("e is", e.target.id)
-    let testMovie = this.props.movies.filter(movie => movie.id === e.target.id)
-    console.log("testMovie:", testMovie)
-    //let newLikes = this.state.likedMovies + 1
-    if(this.state[e.target.id] != undefined) {
-      newLikes = this.state[e.target.id] + 1
-  } else {
-    newLikes = 1
-  }
-  
-      this.setState({
-      //likedMovies: {
-        [e.target.id]: newLikes
-      })
-      console.log("this.state[e.target.id]", this.state[e.target.id])
-      console.log("newLikes", newLikes)
-    //}
-        //...this.state.likedMovies,
-        //id: e.target.id,
-        //count: newLikes
-      //}
-    }
+    let newLikesCount
+    console.log("e is", e.target.id)    
     
+    if(this.state[e.target.id] != undefined) {
+      newLikesCount = this.state[e.target.id] + 1
+    } else {
+      newLikesCount = 1
+    }
+  
+    this.setState({
+      [e.target.id]: newLikesCount
+    })
+    console.log("this.state[e.target.id]", this.state[e.target.id])
+    console.log("newLikes", newLikesCount)
+    
+  }    
   
 
   render() {  
-    console.log("movies state", this.state)
-    //console.log("this.state[2]", this.state[2])
-    
+       
     if(this.props.movies) {
       
       return (    
@@ -60,8 +40,8 @@ class Movies extends Component {
 
           {this.props.movies.map(movie => 
             <li key={movie.id} >
-              <Link to={`/movies/${movie.id}`}>{movie.attributes.title}</Link>
-          <button size="sm" id={movie.id} onClick={this.handleLikes}>Likes: 0 </button>  {/*{this.state[`${movie.id}`].value} ||*/}
+              <Link to={`/movies/${movie.id}`}>{movie.attributes.title}...</Link>
+              <button size="sm" id={movie.id} onClick={this.handleLikes}>Likes:{this.state[`${movie.id}`]|| 0} </button>  
             </li>)} 
         </div>    
       )
